d94f19eca5e5001ff81129680399d83269bda808
Myroslav Raks <raksmyroslav@gmail.com>
2024-06-15 09:45:15 +0200

    clean

--- js/task-3.js
+++
@@ -1,7 +0,0 @@
-document
-  .getElementById('name-input')
-  .addEventListener('input', function (event) {
-    const input = event.target.value.trim();
-    const output = document.getElementById('name-output');
-    output.textContent = input === '' ? 'Anonymous' : input;
-  });
