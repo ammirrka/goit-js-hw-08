dcbb99cc02f5e50f344323668953705e122dcd45
Myroslav Raks <raksmyroslav@gmail.com>
2024-07-24 10:14:43 +0200

    clear

--- js/task-1.js
+++
@@ -1,9 +0,0 @@
-const items = document.querySelectorAll('.item');
-const quantity = items.length;
-console.log(`Number of categories: ${quantity}`);
-const itemInfo = items.forEach(item => {
-  const title = item.querySelector('h2').textContent;
-  const elementsOfItem = item.querySelectorAll('li').length;
-  console.log(`Category: ${title}`);
-  console.log(`Elements: ${elementsOfItem}`);
-});
--- js/task-3.js
+++
@@ -1,6 +0,0 @@
-const input = document.querySelector('#name-input');
-const span = document.querySelector('#name-output');
-input.addEventListener('input', () => {
-  span.textContent =
-    input.value.trim() === '' ? 'Anonymous' : input.value.trim();
-});
